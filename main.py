6e5d18896389ffcae69378046cd4161db714e759
Angel Felipe Magnossao de Paula <angel.magnossao@gmail.com>
2023-09-21 15:59:47 +1000

    add graph script draft 1

--- main.py
+++ main.py
@@ -11,4 +11,153 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-print(wp.summary("data science")[:100])
+class RelationshipGenerator():
+    """Generates relationships between terms, based on wikipedia links"""
+    def __init__(self):
+        """Links are directional, start + end, they should also have a weight"""
+        self.links = [] # [start, end, weight]
+
+    def scan(self, start=None, repeat=0):
+        """Start scanning from a specific word, or from internal database
+        
+        Args:
+            start (str): the term to start searching from, can be None to let
+                algorithm decide where to start
+            repeat (int): the number of times to repeat the scan
+        """
+        while repeat >= 0:
+
+            # should check if start page exists
+            # and haven't already scanned
+            if start in [l[0] for l in self.links]:
+                raise Exception("Already scanned")
+
+            term_search = True if start is not None else False
+
+            # If a start isn't defined, we should find one
+            if start is None: 
+                start = self.find_starting_point()
+
+            # Scan the starting point specified for links
+            print(f"Scanning page {start}...")
+            try:
+                # Fetch the page through the Wikipedia API
+                page = wp.page(start)
+                links = list(set(page.links))
+                # ignore some uninteresting terms
+                links = [l for l in links if not self.ignore_term(l)]
+
+                # Add links to database
+                link_weights = []
+                for link in links:
+                    weight = self.weight_link(page, link)
+                    link_weights.append(weight)
+                
+                link_weights = [w / max(link_weights) for w in link_weights]
+
+                for i, link in enumerate(links):
+                    self.links.append([start, link.lower(), link_weights[i] + 2 * int(term_search)]) # 3 works pretty well
+
+                # Print some data to the user on progress
+                explored_nodes = set([l[0] for l in self.links])
+                explored_nodes_count = len(explored_nodes)
+                total_nodes = set([l[1] for l in self.links])
+                total_nodes_count = len(total_nodes)
+                new_nodes = [l.lower() for l in links if l not in total_nodes]
+                new_nodes_count = len(new_nodes)
+                print(f"New nodes added: {new_nodes_count}, Total Nodes: {total_nodes_count}, Explored Nodes: {explored_nodes_count}")
+
+            except (DisambiguationError, PageError):
+                # This happens if the page has disambiguation or doesn't exist
+                # We just ignore the page for now, could improve this
+                self.links.append([start, "DISAMBIGUATION", 0])
+
+            repeat -= 1
+            start = None
+        
+    def find_starting_point(self):
+        """Find the best place to start when no input is given"""
+        # Need some links to work with.
+        if len(self.links) == 0:
+            raise Exception("Unable to start, no start defined or existing links")
+                
+        # Get top terms
+        res = self.rank_terms()
+        sorted_links = list(zip(res.index, res.values))
+        all_starts = set([l[0] for l in self.links])
+
+        # Remove identifiers (these are on many Wikipedia pages)
+        all_starts = [l for l in all_starts if '(identifier)' not in l]
+        
+        # print(sorted_links[:10])
+        # Iterate over the top links, until we find a new one
+        for i in range(len(sorted_links)):
+            if sorted_links[i][0] not in all_starts and len(sorted_links[i][0]) > 0:
+                return sorted_links[i][0]
+        
+        # no link found
+        raise Exception("No starting point found within links")
+        return
+
+    @staticmethod
+    def weight_link(page, link):
+        """Weight an outgoing link for a given source page
+        
+        Args:
+            page (obj): 
+            link (str): the outgoing link of interest
+        
+        Returns:
+            (float): the weight, between 0 and 1
+        """
+        weight = 0.1
+        
+        link_counts = page.content.lower().count(link.lower())
+        weight += link_counts
+        
+        if link.lower() in page.summary.lower():
+            weight += 3
+        
+        return weight
+
+
+    def get_database(self):
+        return sorted(self.links, key=lambda x: -x[2])
+
+
+    def rank_terms(self, with_start=False):
+        # We can use graph theory here!
+        # tws = [l[1:] for l in self.links]
+        df = pd.DataFrame(self.links, columns=["start", "end", "weight"])
+
+        if with_start:
+            df = df.append(df.rename(columns={"end": "start", "start":"end"}))
+        
+        return df.groupby("end").weight.sum().sort_values(ascending=False)
+    
+    def get_key_terms(self, n=20):
+        return "'" + "', '".join([t for t in self.rank_terms().head(n).index.tolist() if "(identifier)" not in t]) + "'"
+
+    @staticmethod
+    def ignore_term(term):
+        """List of terms to ignore"""
+        if "(identifier)" in term or term == "doi":
+            return True
+        return False
+    
+    
+if __name__ == "__main__":
+    
+    rg = RelationshipGenerator()
+    rg.scan("Public Policy")
+    rg.scan()
+    
+    print('$'*100)
+    print(rg.rank_terms().head(5))
+    
+    print('*'*100)
+    df = pd.DataFrame(rg.links, columns=["start", "end", "weight"])
+    print(df)
+    
+    print('%'*100)
+    print(rg.get_key_terms())
