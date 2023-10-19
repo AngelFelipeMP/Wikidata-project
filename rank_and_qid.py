1475e5120ce006286e185db19d86841995441d3a
Angel Felipe Magnossao de Paula <angel.magnossao@gmail.com>
2023-10-20 10:39:19 +1100

    add script to grab Qids and rank graph nodes

---
+++ rank_and_qid.py
@@ -0,0 +1,36 @@
+# Data manipulation
+import pandas as pd
+
+# Wikipedia API
+# import wikipedia as wp
+
+# Configuration
+import config
+import requests
+import icecream as ic
+
+def get_wikidata_qid(page_title, language="en"):
+    endpoint_url = f"https://{language}.wikipedia.org/w/api.php"
+
+    params = {
+        'action': 'query',
+        'prop': 'pageprops',
+        'titles': page_title,
+        'format': 'json'
+    }
+
+    response = requests.get(endpoint_url, params=params)
+    data = response.json()
+
+    pages = data.get('query', {}).get('pages', {})
+    for _, page_data in pages.items():
+        return page_data.get('pageprops', {}).get('wikibase_item')
+    
+if __name__ == "__main__":
+
+    df_graph = pd.read_csv(config.LOGS_PATH + '/' + 'links.csv')
+    df_rank = df_graph.groupby('end').weight.sum().sort_values(ascending=False).reset_index()
+    df_rank['Qid'] = df_rank['end'].apply(lambda x: get_wikidata_qid(x))
+    df_rank.to_csv(config.LOGS_PATH + '/' + 'links_plus_qids.csv', index=False)
+
+    print('Finished!')
