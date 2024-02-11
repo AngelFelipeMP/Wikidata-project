75f96abe58127257857f940470756daea920e0ce
Angel Felipe Magnossao de Paula <angel.magnossao@gmail.com>
2024-02-12 10:49:14 +1100

    collect_wiki_pages.py v2 - clean comments and and correct access_token

--- collect_wiki_pages.py
+++ collect_wiki_pages.py
@@ -3,38 +3,36 @@ import pandas as pd
 from icecream import ic
 from tqdm import tqdm
 from datetime import datetime
+from dotenv import load_dotenv
 import requests
 import json
 import csv
 import sys
-import os 
-
-#TODO: what todo with the "redirect_target"
-##TODO: clean comments
+import os
 
 def wiki_web_crawler(wiki_pages):
         for page in tqdm(wiki_pages):
             page_title = page.replace(' ', '_')
             page_text = request_wiki_page(page_title)
             
-            # if "errorKey" not in page_text:
             if "source" in page_text:
-                save_wiki_page(page_title, page_text)
+                save_wiki_page(page_text)
             
             
-def save_wiki_page(page_title, page_text):
-    with open(WIKI_PAGES + '/' + page_title + '.json', 'w', encoding='utf-8') as file:
+def save_wiki_page(page_text):
+    with open(WIKI_PAGES + '/' + page_text['key'] + '.json', 'w', encoding='utf-8') as file:
         file.write(json.dumps(page_text, ensure_ascii=False, indent=4))
 
 def save_not_fund_pages(page, error):
-    with open(WIKI_PAGES + '/' + 'not_found_wiki_pages' + '.csv', 'a+', newline='', encoding='utf-8') as file:
+    with open(LOGS_WIKI_PAGES + '/' + 'not_found_wiki_pages' + '.csv', 'a+', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow([page, error])
     
 def request_wiki_page(page_title):
     try:
         url = 'https://api.wikimedia.org/core/v1/wikipedia/en/page/' + page_title
-        access_token = os.environ.get('WIKIPEDIA_ACCESS_TOKEN')
+        load_dotenv()
+        access_token = os.getenv('WIKIPEDIA_ACCESS_TOKEN')
         user_agent = 'Wikipedia_Crawler (angel.magnossao@gmail.com)'
         
         headers = {
@@ -42,12 +40,14 @@ def request_wiki_page(page_title):
             'User-Agent': user_agent}
         
         response = requests.get(url, headers=headers)
-        # ic(response.status_code)
         wiki_json = response.json()
         
-        # if "errorKey" in wiki_json:
         if not "source" in wiki_json:
             save_not_fund_pages(page_title, wiki_json)
+            
+        elif 'redirect_target' in wiki_json:
+            wiki_json = request_wiki_page(wiki_json['redirect_target'].split('/')[-1])
+            
         return wiki_json
         
     except Exception as e:
@@ -117,7 +117,7 @@ if __name__ == "__main__":
     
     #Load not fund wiki pages from a csv file and crawl those pages
     for i in range(1,4):
-        path_not_found_pages = WIKI_PAGES + '/' + 'not_found_wiki_pages' + '.csv'
+        path_not_found_pages = LOGS_WIKI_PAGES + '/' + 'not_found_wiki_pages' + '.csv'
         if os.path.exists(path_not_found_pages):
             tqdm.write(f'Trying to recover lost wikipedia pages: {i}')
             
@@ -134,9 +134,9 @@ if __name__ == "__main__":
     print(f"Number of JSON files in the directory: {json_count} out of 1100")
     
     # Print number of json file without "source" key
-    no_source_key = check_json_files(WIKI_PAGES, 'files_without_source_', 'source', False)
+    no_source_key = check_json_files(LOGS_WIKI_PAGES, 'files_without_source_', 'source', False)
     print(f"Number of JSON files without SOURCE key: {no_source_key}")
     
     # Print number of json file without "source" key
-    redirect_target_key = check_json_files(WIKI_PAGES, 'files_with_redirect-target_', 'redirect_target', True)
+    redirect_target_key = check_json_files(LOGS_WIKI_PAGES, 'files_with_redirect-target_', 'redirect_target', True)
     print(f"Number of JSON files with REDIRECT_TARGET key: {redirect_target_key}")
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ CODE_PATH = os.getcwd()
 REPO_PATH = '/'.join(CODE_PATH.split('/')[0:-1])
 LOGS_PATH = REPO_PATH + '/' + 'logs'
 WIKI_PAGES = REPO_PATH + '/''wiki_pages'
+LOGS_WIKI_PAGES = WIKI_PAGES + '/' + 'logs'
 GRAPH_FILE = 'links.csv'
 PLOT_FILE = 'my_graph.png'
 
