10711991ae95f344e5bcf72f32d684c0aac5b76f
Angel Felipe Magnossao de Paula <angel.magnossao@gmail.com>
2024-02-20 17:03:03 +1100

    script extrac_refs_v5.py - testing 1

--- extract_refs_v5.py
+++ extract_refs_v5.py
@@ -3,16 +3,119 @@ import pandas as pd
 from icecream import ic
 from tqdm import tqdm
 from datetime import datetime
-from wikiciteparser.parser import parse_citation_template
 import mwparserfromhell
+# from wikiciteparser.parser import parse_citation_templat
 import json
 import csv
 import sys
 import os
+import re
 
+# def remove(ref_list, identifiers=True):
+#     ref_list_no_ref_templates = []
+#     pattern_citation = r'(\{\{ *(?!citation needed)citation|\{\{ *cite|\{\{ *wikicite|\{\{ *vcite|http://)' #{{Webarchive
+#     pattern_identifiers = r'(\{\{ *DOI|\{\{ *SSRN|\{\{ *ISBN|\{\{ *ISSN|\{\{ *PMC|\{\{ *PMID)'
+    
+#     for j, ref in enumerate(ref_list):
+#         print(f'{j}) {ref}')
+#         wikicode = mwparserfromhell.parse(ref)
+#         templates = wikicode.filter_templates()
+#         if templates:
+#             for temp in templates:
+#                 if re.search(pattern_citation, temp.__str__(), re.IGNORECASE):
+#                     ref_list_no_ref_templates.append(temp)
+#                     print(f'{j}) {temp}')
+#                 elif identifiers:
+#                     if re.search(pattern_identifiers, temp.__str__(), re.IGNORECASE):
+#                         ref_list_no_ref_templates.append(ref)
+#                         print(f'{j}) <TEXT>')
+#                     else:
+#                         print(f'{j}) <NO-REFE-TEMPLATE> {temp}')
+#                 else:
+#                     print(f'{j}) <NO-REFE-TEMPLATE> {temp}')
+#         else:
+#             ref_list_no_ref_templates.append(ref)
+#             print(f'{j}) <NO-TEMPLATE>')
+#     exit()
+#     return ref_list_no_ref_templates
+
+
+# def extractor_ref_sections(text):
+#     pattern = r'(?<!=)(==\s*(?:References?|Citations?|Sources?|Bibliograph(?:y|ies)|Notes?)\s*==.*?)(?=[^=]==\s*\w|$)'
+#     text_list = re.findall(pattern, text, re.IGNORECASE | re.DOTALL)
+#     ref_list = []
+#     # ic(text_list)
+#     print(repr(text_list))  #
+#     pattern_text_ref = r'\*\s*\w.*?(?=\*|==)'
+#     for j, ref_section in enumerate(text_list):
+#         print(f'-> {j} \n' )
+#         ic(ref_section)
+#         list_refs = re.findall(pattern_text_ref, ref_section, re.IGNORECASE)
+#         ic(list_refs)
+#         ref_list.extend(list_refs)
+#         for ref in list_refs:
+#             ic(ref)
+#     exit()
+#     return ref_list,len(ref_list)
+        
 def ref_extractor(source_text):
-    pass
-    return refs
+    ref_list_tags, _, source_text_off_tags = extractor_ref_between_tags(source_text)
+    ref_list_off_tags_only_templates, _, source_text_off_tags_templates = extractor_ref_template(source_text_off_tags)
+    ref_list_only_ref_sections, _ = extractor_ref_sections(source_text_off_tags_templates)
+    return ref_list_tags + ref_list_off_tags_only_templates + ref_list_only_ref_sections
+
+
+# def extractor_ref_between_tags(text, start=1):
+def extractor_ref_between_tags(text, start=1):
+    text = text.replace("\n", " ")
+    pattern = r'<ref[^/]*?>.*?/ref>'
+    ref_list = re.findall(pattern, text, re.DOTALL)
+    page_off_tags = re.sub(pattern, '', text)
+    ref_list_off_no_ref_templates = remove_no_ref_templates(ref_list)
+    return ref_list_off_no_ref_templates,len(ref_list_off_no_ref_templates), page_off_tags
+
+##TODO: check the who extraction func
+##TODO: Test the hole code
+['page', 'ref_type', 'ref_extracted', 'template', 'ref_no_template']
+def remove_no_ref_templates(ref_list, identifiers=True):
+    ref_list_no_ref_templates = []
+    pattern_citation = r'(\{\{ *(?!citation needed)citation|\{\{ *cite|\{\{ *wikicite|\{\{ *vcite|http://)' #{{Webarchive
+    pattern_identifiers = r'(\{\{ *DOI|\{\{ *SSRN|\{\{ *ISBN|\{\{ *ISSN|\{\{ *PMC|\{\{ *PMID)'
+    
+    for ref in ref_list:
+        wikicode = mwparserfromhell.parse(ref)
+        templates = wikicode.filter_templates()
+        if templates:
+            for temp in templates:
+                if re.search(pattern_citation, temp.__str__(), re.IGNORECASE):
+                    ref_list_no_ref_templates.append(('template', ref, temp.__str__(), None))
+                elif identifiers:
+                    if re.search(pattern_identifiers, temp.__str__(), re.IGNORECASE):
+                        ref_list_no_ref_templates.append(('template', ref, temp.__str__(), None))
+        else:
+            ref_list_no_ref_templates.append(('text',ref, None, None))
+    return ref_list_no_ref_templates
+        
+def extractor_ref_template(text):
+    wikicode = mwparserfromhell.parse(text)
+    templates = wikicode.filter_templates()
+    templates_no_ref_templates_off = remove_no_ref_templates(list(templates), identifiers=False)
+    # remove ref templates from source text
+    for ref_template in templates_no_ref_templates_off:
+        text = text.replace(ref_template, '')
+
+    return templates_no_ref_templates_off,len(templates_no_ref_templates_off), text
+
+
+def extractor_ref_sections(text):
+    pattern = r'(?<!=)(==\s*(?:References?|Citations?|Sources?|Bibliograph(?:y|ies)|Notes?)\s*==.*?)(?=[^=]==\s*\w|$)'
+    text_list = re.findall(pattern, text, re.IGNORECASE | re.DOTALL)
+    ref_list = []
+    pattern_text_ref = r'\*\s*\w.*?(?=\*|==)'
+    for ref_section in text_list:
+        ref_list.extend(re.findall(pattern_text_ref, ref_section, re.IGNORECASE))
+    return ref_list,len(ref_list)
+
 
 def unwind_dict(d):
     try:
@@ -79,11 +182,12 @@ if __name__ == "__main__":
     df_ref_number = pd.DataFrame([],   columns=count_ref_columns)
     
     
-    for file_name in tqdm(os.listdir(WIKI_PAGES + '_test'), desc='EXTRACT-REFS', position=0): ##DEBUG
+    for file_name in tqdm(sorted(os.listdir(WIKI_PAGES + '_test'))[6:], desc='EXTRACT-REFS', position=0): ##DEBUG
         if file_name.endswith(".json"):
             with open(WIKI_PAGES + '_test' + '/' + file_name, 'r', encoding='utf-8') as file: ##DEBUG
                 json_wiki_page = json.load(file)
             
+            ic(json_wiki_page['title'])
             refs = ref_extractor(json_wiki_page['source'])
             refs = [(json_wiki_page['title'],) + ref for ref in refs] ##Remove if classs
             
